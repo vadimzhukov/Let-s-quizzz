d710d2ad0ee39cc1bf77e7404cca4295b023b17b
Vadim <vadim.zhukov@gmail.com>
2022-03-18 17:44:34 +0300

    forgotten index and the end files ;)

---
+++ end.js
@@ -0,0 +1,36 @@
+const userNameText = document.getElementById('userName');
+const saveScoreBtn = document.getElementById('saveScoreBtn');
+const finalScore = document.getElementById('finalScore');
+const mostRecentScore = localStorage.getItem('score');
+
+const highScore = JSON.parse(localStorage.getItem("highScore")) || [];
+const MAX_HIGH_SCORES = 5;
+
+finalScore.innerText = mostRecentScore;
+
+
+
+userNameText.addEventListener('keyup', () => {
+    saveScoreBtn.disabled = !userNameText;
+});
+
+saveHighScore = (e) => {
+     
+    e.preventDefault();
+    
+    const score = {
+        score: mostRecentScore,
+        name: userNameText.value
+    }
+    
+    highScore.push(score);
+    highScore.sort( (a,b) => b.score - a.score);
+    highScore.splice(5);
+    console.log(highScore);
+    
+    localStorage.setItem('highScore', JSON.stringify(highScore));
+    window.location.assign('/');
+};
+
+
+
